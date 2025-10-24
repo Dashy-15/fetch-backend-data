6580b2daf5577b3ccaae05dd31b5478193dce353
Dashy-15 <slowsteady0412@gmail.com>
2025-10-25 01:23:11 +0530

    handled errors

--- src/MoviesPage/MoviesPage.js
+++ src/MoviesPage/MoviesPage.js
@@ -1,37 +1,85 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./MoviesPage.css";
 
+// function MoviesPage() {
+//   const [movies, setMovies] = useState([]);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [error, setError] = useState(null);
+
+//   const fetchMoviesHandler = () => {
+//     setIsLoading(true);
+//     setError(null);
+
+//     fetch("https://swapi.dev/api/films/")
+//       .then((response) => {
+//         if (!response.ok) {
+//           throw new Error("Something went wrong!");
+//         }
+//         return response.json();
+//       })
+//       .then((data) => {
+//         const transformedMovies = data.results.map((movieData) => ({
+//           id: movieData.episode_id,
+//           title: movieData.title,
+//           openingText: movieData.opening_crawl,
+//           releaseDate: movieData.release_date,
+//         }));
+//         setMovies(transformedMovies);
+//         setIsLoading(false);
+//       })
+//       .catch((err) => {
+//         setError(err.message);
+//         setIsLoading(false);
+//       });
+//   };
+
+
+// async-await
 function MoviesPage() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [isRetrying, setIsRetrying] = useState(false);
 
-  const fetchMoviesHandler = () => {
+  const retryTimeoutRef = useRef(null);
+
+  async function fetchMoviesHandler() {
     setIsLoading(true);
     setError(null);
 
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Something went wrong!");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData) => ({
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        }));
-        setMovies(transformedMovies);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        setError(err.message);
-        setIsLoading(false);
-      });
-  };
+    try {
+      const response = await fetch("https://swapi.dev/api/film/");
+      if (!response.ok) {
+        throw new Error("Something went wrong ....Retrying");
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => ({
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      }));
+
+      setMovies(transformedMovies);
+      setIsRetrying(false);
+    } catch (err) {
+      setError(err.message);
+      setIsRetrying(true);
+      retryTimeoutRef.current = setTimeout(fetchMoviesHandler, 5000);
+    }
+    setIsLoading(false);
+  }
+
+  function stopRetryingHandler() {
+    if (retryTimeoutRef.current) {
+      clearTimeout(retryTimeoutRef.current);
+      retryTimeoutRef.current = null;
+    }
+    setIsRetrying(false);
+    setError("Retrying stopped by user.");
+  }
 
   return (
     <div className="movies-container">
@@ -43,6 +91,10 @@ function MoviesPage() {
 
       {isLoading && <p>Loading...</p>}
       {error && <p style={{ color: "red" }}>{error}</p>}
+
+      {isRetrying && (<button className="fetch-btn" onClick={stopRetryingHandler}>
+        Cancel
+      </button>)}
 
       {!isLoading && movies.length > 0 && (
         <ul className="movies-list">
